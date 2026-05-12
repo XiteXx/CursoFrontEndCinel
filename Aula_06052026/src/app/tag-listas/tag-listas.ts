5e8742ed244cbcbd29a2892ad421e22ef696b28b
XiteXx <monteirov@outlook.pt>
2026-05-12 22:49:26 +0100

    Tags
    HTML E javascript de cada tag

--- Aula_06052026/src/app/tag-listas/tag-listas.ts
+++ Aula_06052026/src/app/tag-listas/tag-listas.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './tag-listas.html',
   styleUrl: './tag-listas.css',
 })
-export class TagListas {}
+export class TagListas {
+
+  StringUnica: string = 'Teste de string única';
+
+  Alunos: string[] = ['Leonardo', 'Xavier', 'Sara', 'António', 'Alonso',"1", this.StringUnica];
+
+}
