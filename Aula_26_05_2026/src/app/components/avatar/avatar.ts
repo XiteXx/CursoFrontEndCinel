02f3bb40d3bcb2ff33b87b6ea551ddc7bc6161f4
XiteXx <monteirov@outlook.pt>
2026-05-26 22:32:30 +0100

    Aula 05 - Conclusão

--- Aula_26_05_2026/src/app/components/avatar/avatar.ts
+++ Aula_26_05_2026/src/app/components/avatar/avatar.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   templateUrl: './avatar.html',
   styleUrl: './avatar.css',
 })
-export class Avatar {}
+export class Avatar {
+  nome: string = 'Leonardo';
+  //nome2:string = '1';
+  //nome3 = true;
+
+  logout() {
+    /* throw new Error('Method not implemented.'); */
+    console.log('You have logged out sucessfuly.');
+  }
+  teste() {
+    /* throw new Error('Method not implemented.'); */
+    console.log('Pre-Logout');
+  }
+}
