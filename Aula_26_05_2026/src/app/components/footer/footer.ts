a4e4b55e71b7687f4d623dc2d944b4f029e7a63d
XiteXx <monteirov@outlook.pt>
2026-05-27 21:36:41 +0100

    Aula05 Concluida

--- Aula_26_05_2026/src/app/components/footer/footer.ts
+++ Aula_26_05_2026/src/app/components/footer/footer.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer implements OnInit, OnDestroy {
+  localData: string;
+
+  constructor() {
+    const agora = DateTime.now();
+    this.localData === agora.toLocaleString(DateTime.DATETIME_FULL);
+  }
+}
