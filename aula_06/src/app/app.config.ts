4c1a13795e34163c642da1f709880c05315dcd1c
XiteXx <monteirov@outlook.pt>
2026-06-02 21:43:47 +0100

    Adição do firebase

--- aula_06/src/app/app.config.ts
+++ aula_06/src/app/app.config.ts
@@ -2,6 +2,11 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environment/environment.prod';
+
+
+export const app = initializeApp(environment.firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
---
+++ aula_06/src/environment/environment.prod.ts
@@ -0,0 +1,26 @@
+import {keys} from './environment.keys';
+
+export const environment = {
+
+  production: true,
+
+  firebaseConfig: {
+
+  apiKey: keys.apiKey,
+
+  authDomain: "cinel-aula6.firebaseapp.com",
+
+  projectId: "cinel-aula6",
+
+  storageBucket: "cinel-aula6.firebasestorage.app",
+
+  messagingSenderId: "255651576175",
+
+  appId: "1:255651576175:web:1f9c0c0629483b6a52909a",
+
+  measurementId: "G-TLEQBF606P"
+
+}
+};
+
+
